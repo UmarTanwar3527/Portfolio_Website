7fbb001cc6b1885ea55cfafae22337e05c5de57e
umartanwar11651913 <umartanwar11651913@gmail.com>
2024-09-06 17:08:54 +0000

    AboutMe Updated

--- lib/View/Components/about_me.dart
+++ lib/View/Components/about_me.dart
@@ -22,7 +22,7 @@ class AboutMe extends StatelessWidget {
             ),
           ),
           Text(
-            "Hello! I'm Nabin Paudayl,a passionate Flutter developer specializing in Flutter, Firebase, API integration, and UI design. With a keen eye for detail and a drive for excellence, I strive to create seamless and engaging mobile applications.\n\nIn addition to my development work, I share my knowledge and expertise through educational videos on YouTube, helping others to learn and grow in the field of Flutter development.\n\nThank you for visiting my portfolio. Feel free to explore my projects and reach out if you'd like to connect!",
+            "Hello! I'm Mohammed Umar Tanwar,a passionate Flutter developer specializing in Flutter, Firebase, API integration, and UI design. With a keen eye for detail and a drive for excellence, I strive to create seamless and engaging mobile applications.\n\nIn addition to my development work, I share my knowledge and expertise through educational post on LinkedIn and Twitter, helping others to learn and grow in the field of Flutter development.\n\nThank you for visiting my portfolio. Feel free to explore my projects and reach out if you'd like to connect!",
             style: TextStyle(
               fontSize: Responsive.isMobile(context) ? 18 : 20,
             ),
